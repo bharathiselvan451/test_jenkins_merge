3458b16a278f5295369358326a33e81d9ec770c6
Bharathiselvan Rajendran <107631601+bharathiselvan451@users.noreply.github.com>
2025-08-10 13:19:34 -0600

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,14 +1,15 @@
 terraform {
   backend "s3" {
-    bucket                  = "terraform-s3-state-0223"
+    bucket                  = "terraform-s3-state-0224"
     key                     = "my-terraform-project"
-    region                  = "us-east-1"
+    dynamodb_table          = "tf-state-lock"
+    region                  = "us-east-2"
     shared_credentials_file = "~/.aws/credentials"
   }
 }
 
 provider "aws" {
-  region                  = "us-east-1"
+  region                  = "us-east-2"
 }
 
 
